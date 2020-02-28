b52f74cbf24341808795db5ce19e87eb587b6b22
kato hawke <katohawke@gmail.com>
2020-02-28 16:52:32 +0200

    Added my own include library // Used for generate an array

---
+++ include/seed/seed.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "seed.h"
+
+double getRandomNum(const double min, const double max) {
+    return ((double) rand() / RAND_MAX) * (max - min + 1) + min;
+}
+
+// Fill elements of array with values in range
+void fillArray(int numOfElements, int *array, const int range[]) {
+    for (int i = 0; i < numOfElements; i++) {
+		array[i] = getRandomNum(range[0], range[1]);
+	}
+}
+
+// Print elements of array
+void printArray(int numOfElements, int *array) {
+    for (int i = 0; i < numOfElements; i++)
+		printf("%d\t", array[i]);
+	
+	printf("\n");
+}
---
+++ include/seed/seed.h
@@ -0,0 +1,5 @@
+double getRandomNum(const double min, const double max);
+
+void fillArray(int numOfElements, int *array, const int range[]);
+
+void printArray(int numOfElements, int *array);
