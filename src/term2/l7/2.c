ab01e28d85fb98216d4861410d2e9d019f725ca7
Kato Hawke <katohawke@gmail.com>
2020-02-29 22:27:59 +0200

    Absolute max element in an array and sum btw 2 positive nums

--- src/term2/l7/2.c
+++ src/term2/l7/2.c
@@ -1,10 +1,3 @@
-/*  Author: Ivan Tkachuk
-    Name: Get max absolute element in array
-        And get sum between two positive numbers
-    Date: 28.02.2020 16:54
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -14,66 +7,60 @@
 #define MIN -10
 #define MAX 10
 
-int getMaxAbsoluteElement(int numOfElements, int *array);
-int getSumModificied(int numOfElements, int *array);
+int abs_max_elem(int length_array, int *array);
+int sum_btw_2_positive_nums(int length_array, int *array);
+int positive(int n);
 
+int main(int argc, char *argv[]) {
+  int length_array, *array;
 
-int main(int argc, char *argv[])
-{
-    int numOfElements, *array;
+  printf("Num of elements: ");
+  scanf("%d", &length_array);
 
-    printf("Num of elements: ");
-    scanf("%d", &numOfElements);
+  srand(time(NULL));
 
-    srand(time(NULL));
+  array = (int *)malloc(length_array * sizeof(int));
+  if (array == NULL) {
+    printf("Can't create\n");
+    exit(1);
+  }
 
-    array = (int *)malloc(numOfElements * sizeof(int));
-    if (!array)
-    {
-        printf("Can't create\n");
-        exit(1);
-    }
+  fillArray(length_array, array, (int[]){MIN, MAX});
+  printArray(length_array, array);
 
-    fillArray(numOfElements, array, (int[]){MIN, MAX});
-    printArray(numOfElements, array);
+  printf("max = %d\n", abs_max_elem(length_array, array));
+  printf("sum = %d\n", sum_btw_2_positive_nums(length_array, array));
 
-    printf("max = %d\n", getMaxAbsoluteElement(numOfElements, array));
-    printf("sum = %d\n", getSumModificied(numOfElements, array));
+  free(array);
 
-    free(array);
-    
-    return 0;
+  return 0;
 }
 
-int getMaxAbsoluteElement(int numOfElements, int *array)
-{
-    int max = array[0];
+int abs_max_elem(int length_array, int *array) {
+  int max = array[0];
 
-    for (int *p = array; numOfElements--; p++)
-    {
-        max = (abs(*p) > abs(max)) ? *p : max;
-    }
+  for (int *p = array; length_array--; p++) {
+    max = (abs(*p) > abs(max)) ? *p : max;
+  }
 
-    return max;
+  return max;
 }
 
-int getSumModificied(int numOfElements, int *array)
-{
-    int sum = 0;
+int sum_btw_2_positive_nums(int numOfElements, int *array) {
+  int sum = 0;
 
-    for (int i = 0; i < numOfElements; i++)
-    {
-        if (*(array + i) > 0)
-        {
-            i++;
-            while(!(*(array + i) > 0))
-            {
-                sum += *(array + i);
-                i++;
-            }
-            break;
-        }
+  for (int i = 0; i < numOfElements; i++) {
+    if (positive(*(array + i))) {
+      i++;
+      while (positive(*(array + i)) == 0) {
+        sum += *(array + i);
+        i++;
+      }
+      break;
     }
+  }
 
-    return sum;
+  return sum;
 }
+
+int positive(int n) { return n > 0; }
