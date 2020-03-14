e3b35b914b86c686e81b7be29a1c1eeb137deec0
Ivan Tkachuk <61608639+katohawkei@users.noreply.github.com>
2020-03-14 14:47:17 +0200

    Added 1.c
    Reformat and comment code

--- src/term2/l7/1.c
+++ src/term2/l7/1.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+// my lib od funcs
+// for work with array
+// Like fill and print
 #include "../../../include/seed/seed.h"
 
+// Random range for fill
 #define MIN 1
 #define MAX 10
 
+// min elem for even position
 int min_elem(int length_array, int *array);
-int even(int n);
+
+// is it even?
+int even(int n) { return n % 2 == 0 };
 
 int main(int argc, char *argv[]) {
   int length_array, *array;
@@ -24,6 +31,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  // My funcs for generate element and print
   fillArray(length_array, array, (int[]){MIN, MAX});
   printArray(length_array, array);
 
@@ -37,13 +45,19 @@ int main(int argc, char *argv[]) {
 int min_elem(int length_array, int *array) {
   int min = array[0];
 
+  // *p = array <=> *p = &array[0]
+  // my pointer var can move everywhere
+  // So I need the stoping move when array ends
+  // Like length_array
   for (int *p = array; length_array--; p++) {
     if (even(length_array)) {
+      // Here is just like 
+      // min = (array[i] < min) ...
+      // But I used pointer var, 
+      // so It's not necessary for using index var
       min = (*p < min) ? *p : min;
     }
   }
 
   return min;
 }
-
-int even(int n) { return n % 2 == 0; }
