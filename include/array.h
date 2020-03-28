415c71924d4d22f2a203af98e186e78e815e6e90
katohawke <katohawke@gmail.com>
2020-03-28 11:49:58 +0200

    Updated for working with double array

--- include/array.h
+++ include/array.h
@@ -2,7 +2,15 @@
 double getRandomNum(const double min, const double max);
 
 // Fill elements of array with values in range
-void fillArray(int numOfElements, int *array, const int range[]);
+void fillArray_i(int *array, int numOfElements, const int range[]);
 
 // Print elements of array
-void printArray(int numOfElements, int *array);
+void printArray_i(int *array, int numOfElements);
+
+int *createArray_i(const int n);
+
+double *createArray_d(const int n);
+
+void fillArray_d(double *array, const int n, double const range[2]);
+
+void printArray_d(double *array, const int n);
